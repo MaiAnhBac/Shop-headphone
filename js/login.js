07fc6d1174da8d79ba5afb6d370aea8d7696194d
Mai Anh Bắc <maianhbac17052001@gmail.com>
2024-03-22 15:42:31 +0700

    update

--- js/login.js
+++ js/login.js
@@ -9,3 +9,17 @@ registerBtn.addEventListener('click', () => {
 loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
 });
+
+const create = document.getElementById('create');
+const sign = document.getElementById('signin');
+const sign_in = document.querySelector('.sign-in')
+const sign_up = document.querySelector('.sign-up')
+
+create.addEventListener('click', () => {
+    sign_in.classList.add("sign-in-active")
+    sign_up.classList.add("sign-up-active")
+})
+sign.addEventListener('click', () => {
+    sign_in.classList.remove("sign-in-active")
+    sign_up.classList.remove("sign-up-active")
+})
