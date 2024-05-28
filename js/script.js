1e69e28733d8d56d18dabc7e59c323016f4259e1
Mai Anh Bắc <maianhbac17052001@gmail.com>
2024-05-28 10:22:39 +0700

    update

--- js/script.js
+++ js/script.js
@@ -1,35 +1,24 @@
-
-const option1 = document.querySelector('#option1');
-const option2 = document.querySelector('#option2');
-const option3 = document.querySelector('#option3');
-const option4 = document.querySelector('#option4');
-const option5 = document.querySelector('#option5');
-
-document.addEventListener("DOMContentLoaded", function() {
-  // Lắng nghe sự kiện click trên radio buttons
-  var radios = document.querySelectorAll('input[type="radio"]');
-  radios.forEach(function(radio) {
-      radio.addEventListener('change', function() {
-          // Kiểm tra xem radio nào được chọn
-          if (option1.checked) {
-              // Nếu radio màu đỏ được chọn, thay đổi màu ảnh thành đỏ
-              document.getElementById('img-brg').style.filter = 'grayscale(0%)';
-          } else if (option2.checked) {
-              // Nếu radio màu xanh được chọn, thay đổi màu ảnh thành xanh
-              document.getElementById('img-brg').style.filter = 'grayscale(100%)';
-          } else if (option3.checked) {
-            // Nếu radio màu xanh được chọn, thay đổi màu ảnh thành xanh
-            document.getElementById('img-brg').style.filter = 'hue-rotate(95deg)';
-          } else if (option4.checked) {
-            // Nếu radio màu xanh được chọn, thay đổi màu ảnh thành xanh
-            document.getElementById('img-brg').style.filter = 'hue-rotate(195deg)';
-          } else if (option5.checked) {
-            // Nếu radio màu xanh được chọn, thay đổi màu ảnh thành xanh
-            document.getElementById('img-brg').style.filter = 'hue-rotate(300deg)';
-        } 
+document.querySelectorAll('.colors_item').forEach(function(item) {
+  item.addEventListener('click', function() {
+      // First, set all items to text-transparent
+      document.querySelectorAll('.colors_item').forEach(function(otherItem) {
+          otherItem.classList.remove('text-white');
+          otherItem.classList.add('text-transparent');
       });
+      
+      // Then, set the clicked item to text-white
+      item.classList.remove('text-transparent');
+      item.classList.add('text-white');
   });
 });
+document.querySelectorAll('.size').forEach((item) => {
+  item.addEventListener('click', () => {
+    document.querySelectorAll('.size').forEach((size) => {
+      size.classList.remove('tick')
+    })
+    item.classList.add('tick')
+  })
+})
 
 //tăng giảm
 document.addEventListener('DOMContentLoaded', function() {
